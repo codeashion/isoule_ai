ed0e1fc1d9e37b16c6cb0cd84f300a3a8ea7d4db
Harsh Sangani <harshsangani001@gmail.com>
2026-02-17 15:24:45 +0530

    done

--- lib/src/core/di.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-
-final GetIt getIt = GetIt.instance;
-
-Future<void> initDependencies() async {
-  // Register repositories, services and singletons here.
-  // Example:
-  // getIt.registerLazySingleton<MyRepository>(() => MyRepositoryImpl());
-}
